469fd875c93da65287da21841341eb8ce9172653
Ritik <sagarritik123@gmail.com>
2023-07-24 00:59:51 +0530

    first commit

---
+++ React-Calculator-master/src/App.js
@@ -0,0 +1,12 @@
+import Calculator from './compoents/calculator';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>CALCULATOR</h1>
+      <Calculator/>
+    </div>
+  );
+}
+
+export default App;
